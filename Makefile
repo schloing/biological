09df81668c983108c0c07a5f37cb8cd22393eb47
schloing <user.datagram.protocol@proton.me>
2023-10-05 19:47:27 +1100

    begin Weiler–Atherton

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
-OBJS   = main.o geometry.o
+OBJS   = main.o  geometry.o
 SOURCE = main.hs geometry.hs
-HEADER = 
 OUT    = out
 CC     = ghc
-FLAGS  = -g -c -Wall
 
 all: $(OBJS)
-	$(CC) -g $(OBJS) -o $(OUT) $(LFLAGS)
+	$(CC) -g $(OBJS) -o $(OUT)
 
 main.o: main.hs
 	$(CC) $(FLAGS) main.hs 
--- geometry.hs
+++ geometry.hs
@@ -1,4 +1,5 @@
 module Geometry ( Point, Line, Polygon ) where
+import Control.Monad (forM_)
 
 data Point = Point
     { x :: Float
@@ -20,9 +21,12 @@ new_line a b
     | a == b    = error "origin and end points are identical"
     | otherwise = Line { a , b }
 
-polygon_intersection :: Polygon -> Polygon -> IO (Polygon)
+polygon_intersection :: Polygon -> Polygon -> IO Polygon
 polygon_intersection a b = do
     let a_size :: Int = length (vertices a)
-    let b_size :: Int = length (vertices b)
+        b_size :: Int = length (vertices b)
 
-    return Polygon { vertices = [] , segments = [] }
+    let handler = print "HI"
+    forM_ (vertices a) (\x -> handler)
+
+    return $ Polygon { vertices = [], segments = [] }
